5613441df0366403df74ebb24d6b09236392633e
gevin <gevinpinto@gmail.com>
2023-08-30 14:08:33 +0530

    div function added

--- pyth_program/arith.py
+++ pyth_program/arith.py
@@ -4,10 +4,13 @@ def sub(n1,n2):
     return(n1-n2)
 def mul(n1,n2):
     return(n1*n2)
+def div(n1,n2):
+    return(n1/n2)
 num1=int(input("enter a number="))
 num2=int(input("enter a number="))
 print("sum="add(num1,num2))
 print("difference="sub(num1,num2))
-print("mul="mul(num1,num2))    
+print("mul="mul(num1,num2))   
+print("div="div(num1,num2)) 
 
 
